27c008e4a2a1e016ca6958929912cad447954ddc
LuCarlo <luquinhabsb96@hotmail.com>
2017-06-26 21:13:45 -0300

    Template

---
+++ src/br/com/cotasmart/teste/TestaConexaoBD.java
@@ -0,0 +1,15 @@
+package br.com.cotasmart.teste;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.com.cotasmart.conexao.ConnectionFactory;
+
+public class TestaConexaoBD {
+
+	public static void main(String[] args) throws SQLException {
+		Connection con = new ConnectionFactory().getConnection();
+		System.out.println("Conectado");
+		con.close();
+	}
+}
