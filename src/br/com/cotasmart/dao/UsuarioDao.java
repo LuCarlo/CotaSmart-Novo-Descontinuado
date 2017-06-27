3ff50fc2330e26d68311336b180678b03a06c120
Carlos Henrique <chtsilva104@gmail.com>
2017-06-26 23:35:05 -0300

    DAO e Servlet do usuário inclusos

--- src/br/com/cotasmart/dao/UsuarioDao.java
+++ src/br/com/cotasmart/dao/UsuarioDao.java
@@ -1,6 +1,7 @@
 package br.com.cotasmart.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import br.com.cotasmart.factory.ConnectionFactory;
@@ -16,7 +17,25 @@ public class UsuarioDao {
 			throw new RuntimeException(e);
 		}
 	}
-	public void adiciona(Usuario usuario){
-
+	public void adiciona(Usuario usuario) {
+		String sql = "INSERT INTO usuarios ("+
+					"nome, login, senha, codStatus, codGrupo) "+
+					"VALUES (?,?,?,?,?) ";
+		try {
+			
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, usuario.getNome());
+			stmt.setString(2, usuario.getLogin());
+			stmt.setString(3, usuario.getSenha());
+			stmt.setLong(4, usuario.getCodStatus());
+			stmt.setLong(5, usuario.getCodGrupo());
+			
+			stmt.execute();
+			
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+		
 	}
 }
