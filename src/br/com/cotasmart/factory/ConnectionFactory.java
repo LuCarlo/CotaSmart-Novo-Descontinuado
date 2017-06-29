c9cb7e1b09205e2ece73bc84ffd66e170fea1e86
Carlos <chtsilva104@gmail.com>
2017-06-29 12:36:37 -0300

    Pequenas alterações na conexão

--- src/br/com/cotasmart/factory/ConnectionFactory.java
+++ src/br/com/cotasmart/factory/ConnectionFactory.java
@@ -5,22 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-
 	public Connection getConnection() throws SQLException {
 		String driver = "org.postgresql.Driver";
 		String usuario = "postgres";
 		String senha = "juizladrao";
-		String url = "jdbc:postgresql://localhost:5432/CotaSmart";
+		String url = "jdbc:postgresql://10.1.1.255:5433/CotaSmart";
 
-		System.out.println("Conectando...");
+		System.out.println("Tentando Conectar....");
 		try {
 			Class.forName(driver);
 		} catch (ClassNotFoundException e) {
+			System.out.println("Erro de conexão(VERIFICAR PORTA): " + e.getMessage());
 			throw new SQLException(e);
 		}
-		System.out.println("Conectado");
+
 		return DriverManager.getConnection(url, usuario, senha);
 	}
-	
-	
+
 }
---
+++ src/br/com/cotasmart/teste/TestarDao.java
@@ -0,0 +1,13 @@
+package br.com.cotasmart.teste;
+
+import br.com.cotasmart.dao.FornecedorDao;
+
+public class TestarDao {
+
+	public static void main(String[] args) {
+		FornecedorDao dao = new FornecedorDao();
+		System.out.println("Conectado");
+
+	}
+
+}
